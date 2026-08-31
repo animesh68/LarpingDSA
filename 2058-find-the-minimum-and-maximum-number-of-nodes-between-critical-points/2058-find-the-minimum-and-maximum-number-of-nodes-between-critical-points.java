0fec8e4aa46fcd959354e5f4ef002523ae6b16a8
Animesh Chandra Srivastava <animeshyt68@gmail.com>
2026-08-31 10:49:46 +0530

    Time: 9 ms (23.01%), Space: 107.9 MB (16.32%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,39 +10,34 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-
-        List<Integer> crit = new ArrayList<>();
-
-        ListNode prev = head;
-        ListNode curr = head.next;
-
-        int pos = 1;
-
-        while (curr.next != null) {
-
-            if ((curr.val > prev.val && curr.val > curr.next.val) ||
-                (curr.val < prev.val && curr.val < curr.next.val)) {
-
-                crit.add(pos);
-            }
-
-            prev = curr;
+        ListNode curr = head;
+        int count = 0;
+        while(curr!=null){
+            count++;
             curr = curr.next;
-            pos++;
         }
 
+        if(count<=2) return new int[]{-1,-1};
+        
+        List<Integer> crit = new ArrayList<>();
+        curr = head.next;
+        ListNode prev = head;
+        count = 0;
+        while(curr.next!=null){
+            if(curr.val < prev.val && curr.val<curr.next.val) crit.add(count);
+            if(curr.val > prev.val && curr.val > curr.next.val) crit.add(count);          
+            prev = curr;
+            curr = curr.next;
+            count++;
+        }
         if (crit.size() < 2) {
             return new int[]{-1, -1};
         }
-
-        int maxDist = crit.get(crit.size() - 1) - crit.get(0);
-
-        int minDist = Integer.MAX_VALUE;
-
-        for (int i = 1; i < crit.size(); i++) {
-            minDist = Math.min(minDist, crit.get(i) - crit.get(i - 1));
+        int maxdist = crit.get(crit.size()-1) - crit.get(0);
+        int mindist = Integer.MAX_VALUE;
+        for(int i=1;i<crit.size();i++){
+            mindist = Math.min(mindist,crit.get(i)-crit.get(i-1));
         }
-
-        return new int[]{minDist, maxDist};
+        return new int[]{mindist,maxdist};
     }
 }
