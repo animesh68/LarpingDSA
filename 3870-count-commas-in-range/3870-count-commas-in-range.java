fcdddc0e06de4eda8a1a49ebc83db21693e4c03e
Animesh Chandra Srivastava <animeshyt68@gmail.com>
2026-09-15 21:53:13 +0530

    Time: 0 ms (100%), Space: 42.4 MB (76.48%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,12 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        int count = 0;
-        int m = n;
-        while(m>0){
-            m = m/10;
-            count++;
-        }
-        if(count <= 3) return 0;
+        if(n <= 999) return 0;
         else return n-999;
     }
 }
