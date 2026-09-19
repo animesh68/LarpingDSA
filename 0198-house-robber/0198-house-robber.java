026085eb8328b82956357fe54533a182074a3b5c
Animesh Chandra Srivastava <animeshyt68@gmail.com>
2026-09-19 10:53:21 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.91%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][2];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return fun(nums,n,0,1,dp);
+    }
+    int fun(int[] nums, int n, int i, int free, int[][] dp){
+        if(i==n) return 0;
+        if(dp[i][free] != -1) return dp[i][free];
+        if(free == 0) return dp[i][free] = fun(nums,n,i+1,1,dp);
+        int c = nums[i] + fun(nums,n,i+1,0,dp);
+        int cc = fun(nums,n,i+1,1,dp);
+        return dp[i][free] = Math.max(c,cc);
+    }
+}
